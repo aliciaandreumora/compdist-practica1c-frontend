03ea48294c96685e3df5186acea23dde38879ea3
aliciaandreumora <aliciaandreumora@gmail.com>
2025-12-13 18:37:21 +0100

    Deploy: frontend Vue Vite

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 import { useAuthStore } from "@/stores/store.js"
 
 import LoginView from './views/LoginView.vue'
@@ -9,31 +9,31 @@ import NoGameView from './views/NoGameView.vue'
 import TresEnRaya from './views/TresEnRaya.vue'
 
 const routes = [
-    { path: '/login', name: 'login', component: LoginView },
-    { path: '/register', name: 'register', component: RegisterView },
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/GameEdit', name: 'game_edit', component: GameEditView },
-    { path: '/NoGame', name: 'no_game', component: NoGameView },
-    { path: '/TresEnRaya', name: 'tres_en_raya', component: TresEnRaya },
+  { path: '/login', name: 'login', component: LoginView },
+  { path: '/register', name: 'register', component: RegisterView },
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/GameEdit', name: 'game_edit', component: GameEditView },
+  { path: '/NoGame', name: 'no_game', component: NoGameView },
+  { path: '/TresEnRaya', name: 'tres_en_raya', component: TresEnRaya },
 ]
 
 export const router = createRouter({
-    // 👇 CAMBIO IMPORTANTE
-    history: createWebHashHistory(),
-    routes,
+  // 👇 muy importante usar la base que Vite inyecta
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 })
 
 router.beforeEach(async (to, from, next) => {
-    const auth = useAuthStore()
-    if (auth.loading) await auth.checkAuth()
+  const auth = useAuthStore()
+  if (auth.loading) await auth.checkAuth()
 
-    const isLoggedIn = !!auth.user
+  const isLoggedIn = !!auth.user
 
-    if (!isLoggedIn && to.name !== "login" && to.name !== "register") {
-        next({ name: "login" })
-    } else if (isLoggedIn && to.name === "login") {
-        next({ name: "home" })
-    } else {
-        next()
-    }
+  if (!isLoggedIn && to.name !== "login" && to.name !== "register") {
+    next({ name: "login" })
+  } else if (isLoggedIn && to.name === "login") {
+    next({ name: "home" })
+  } else {
+    next()
+  }
 })
